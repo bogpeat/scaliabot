a89d4e347559d4fe2017a97d3397ac14e4fe903b
Sophia Chan <mngschan@gmail.com>
2018-07-08 23:21:10 +0000

    cleaned up

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const Discord = require('discord.js');
 const secrets = require('./secrets');
 const config = require('./config');
 
+function randomElementOf(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
 
 client.on('message', msg => {
   function hasMagicWord(magicWord) {
@@ -15,7 +18,7 @@ client.on('message', msg => {
   }
   if (config.MAGIC_WORDS.find(hasMagicWord) !== undefined) {
     msg.reply('', {
-      files: [config.IMAGES[Math.floor(Math.random() * config.IMAGES.length)]],
+      files: [randomElementOf(config.IMAGES)],
     });
   }
 });
